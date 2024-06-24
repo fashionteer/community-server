fca9039fc42c8e0ad2ad9fe8e3c5e226ad3bccbc
hainho <hainho9704@gmail.com>
2024-06-24 20:19:57 +0900

    ✅ /sign-up path 테스트 코드 작성

---
+++ src/test/java/com/fashionteer/application/account/endpoint/controller/AccountControllerTest.java
@@ -0,0 +1,31 @@
+package com.fashionteer.application.account.endpoint.controller;
+
+import com.fashionteer.application.Application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("회원 가입 폼 확인")
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andDo(print());
+    }
+}
