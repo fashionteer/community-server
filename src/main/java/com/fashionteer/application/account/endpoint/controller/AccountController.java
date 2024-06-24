712e8199a9f81ece63b135309175e167eb821520
hainho <hainho9704@gmail.com>
2024-06-24 20:14:48 +0900

    ✨ accountController 에 sign-up 추가

---
+++ src/main/java/com/fashionteer/application/account/endpoint/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.fashionteer.application.account.endpoint.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String signUpForm() {
+        return "account/sign-up";
+    }
+}
