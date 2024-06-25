cb46262fba160263876f9d50b5792086c24fdd6f
hainho <hainho9704@gmail.com>
2024-06-25 23:02:19 +0900

    ✨ signUpForm 클래스에 validation 적용

--- src/main/java/com/fashionteer/application/account/endpoint/controller/form/SignUpForm.java
+++ src/main/java/com/fashionteer/application/account/endpoint/controller/form/SignUpForm.java
@@ -1,10 +1,21 @@
 package com.fashionteer.application.account.endpoint.controller.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class SignUpForm {
+    @NotBlank
+    @Length(min = 2, max = 20)
     private String nickname;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Length(min = 8, max = 20)
     private String password;
 }
