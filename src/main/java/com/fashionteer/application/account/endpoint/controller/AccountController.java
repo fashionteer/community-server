87919eda78723d76f7e66f0cc609ba48c7a3e9df
hainho <hainho9704@gmail.com>
2024-06-26 23:24:40 +0900

    ✨ 컨트롤러에 post /sign-up 추가

--- src/main/java/com/fashionteer/application/account/endpoint/controller/AccountController.java
+++ src/main/java/com/fashionteer/application/account/endpoint/controller/AccountController.java
@@ -1,9 +1,13 @@
 package com.fashionteer.application.account.endpoint.controller;
 
 import com.fashionteer.application.account.endpoint.controller.form.SignUpForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
@@ -13,4 +17,16 @@ public class AccountController {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(
+            @Valid @ModelAttribute SignUpForm signUpForm,
+            Errors errors)
+    {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        return "redirect:/";
+    }
+
 }
