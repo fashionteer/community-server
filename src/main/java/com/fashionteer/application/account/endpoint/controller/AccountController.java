5563a0f507859b79de131f8abfc127720860edb6
hainho <hainho9704@gmail.com>
2024-06-26 23:26:42 +0900

    ✨ sign up validator 추가

--- src/main/java/com/fashionteer/application/account/endpoint/controller/AccountController.java
+++ src/main/java/com/fashionteer/application/account/endpoint/controller/AccountController.java
@@ -1,16 +1,28 @@
 package com.fashionteer.application.account.endpoint.controller;
 
 import com.fashionteer.application.account.endpoint.controller.form.SignUpForm;
+import com.fashionteer.application.account.endpoint.controller.validator.SignUpFormValidator;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
