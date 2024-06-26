0d4a33d808329f45495183460461c7f518ad0c93
hainho <hainho9704@gmail.com>
2024-06-27 01:32:48 +0900

    ✅ sign-up 테스트 코드 작성

--- src/test/java/com/fashionteer/application/account/endpoint/controller/AccountControllerTest.java
+++ src/test/java/com/fashionteer/application/account/endpoint/controller/AccountControllerTest.java
@@ -1,14 +1,17 @@
 package com.fashionteer.application.account.endpoint.controller;
 
 import com.fashionteer.application.Application;
-import org.junit.jupiter.api.DisplayName;
+import com.fashionteer.application.account.infra.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,13 +22,53 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Test
-    @DisplayName("회원 가입 폼 확인")
-    void signUpForm() throws Exception {
+    void 회원가입_폼_확인() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"))
                 .andDo(print());
+    }
+
+    @Test
+    void 회원가입_실패_이메일_형식_오류() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "nickname")
+                        .param("email", "email")
+                        .param("password", "12345678")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @Test
+    void 회원가입_실패_비밀번호_형식_오류() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "nickname")
+                        .param("email", "email@email.com")
+                        .param("password", "1234")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @Test
+    void 회원가입_성공() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "nickname")
+                        .param("email", "email@email.com")
+                        .param("password", "12345678")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        assertTrue(accountRepository.existsByEmail("email@email.com"));
     }
 }
